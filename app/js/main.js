7a3b05491281cc66be6c27c2e67d877b91748429
OvcharovCoder <ovcharovcoder@gmail.com>
2025-02-20 15:19:57 +0200

    Added Catalog section

--- app/js/main.js
+++ app/js/main.js
@@ -44,3 +44,37 @@ $('.product-slider').slick({
 	nextArrow:
 		'<button class="product-slider__slider-btn product-slider__slider-btn-next"><img src="img/icons/arrow-black-right.svg" alt="arrow-right"></button>',
 });
+
+// Filter
+$('.filter-style').styler();
+
+$('.filter__item-drop-title').on('click', function () {
+	$(this).toggleClass('filter__item-drop-title--active');
+	$(this).next().slideToggle();
+});
+
+// RangeSlider plugin
+$('.js-range-slider').ionRangeSlider({
+	// Приховує значення над повзунком
+});
+
+// Filter Grid or List
+$('.catalog__filter-btn-grid').on('click', function () {
+	$(this).addClass('catalog__filter-btn--active');
+	$('.catalog__filter-btn-list').removeClass('catalog__filter-btn--active');
+
+	$('.catalog__inner-list').removeClass('catalog__inner-list--list');
+	$('.catalog__inner-list .product-item__wrapper').removeClass(
+		'product-item__wrapper--list'
+	);
+});
+
+$('.catalog__filter-btn-list').on('click', function () {
+	$(this).addClass('catalog__filter-btn--active');
+	$('.catalog__filter-btn-grid').removeClass('catalog__filter-btn--active');
+
+	$('.catalog__inner-list').addClass('catalog__inner-list--list');
+	$('.catalog__inner-list .product-item__wrapper').addClass(
+		'product-item__wrapper--list'
+	);
+});
